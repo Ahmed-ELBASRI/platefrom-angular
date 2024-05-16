f8f009fbe03711a18e318da54239b117aada644d
AYOUB BOUSSAIDI <Boussaidi-Ayoub@hotmail.com>
2024-05-16 12:15:58 +0100

    add register function

---
+++ src/app/Dtos/registerDto.ts
@@ -0,0 +1,6 @@
+export class registerDto{
+  nom_Venduer! : string;
+  prenom_Vendeur! : string ;
+  email_Vendeur!  : string ;
+  password!  : string;
+}
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {LoginDto} from "./Dtos/loginDto";
+import {registerDto} from "./Dtos/registerDto";
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,10 @@ export class AuthService {
 
   login(loginDto: LoginDto): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/LoginVendeur`, loginDto);
+  }
+
+  register(registerDto : registerDto) : Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/LoginVendeur/register`, registerDto);
   }
 
 
