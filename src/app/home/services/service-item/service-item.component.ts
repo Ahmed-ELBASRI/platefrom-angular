6d582be29d826a78876c363cdc15d3682d65c88b
Medovsky <ahmed123elbasri@gmail.com>
2024-05-25 23:27:17 +0100

    fix design

--- src/app/home/services/service-item/service-item.component.ts
+++ src/app/home/services/service-item/service-item.component.ts
@@ -8,4 +8,11 @@ import { Component, Input } from '@angular/core';
 export class ServiceItemComponent {
   @Input() plan :string="";
   @Input() planDescription :string="";
+  @Input() storesetup :string="";
+  @Input() products :string="";
+  @Input() storage :string="";
+  @Input() support :string="";
+  @Input() price :string="";
+  @Input() shortDescription :string="";
+
 }
